cc7497f716bfdd5acac0bcad7a34cd7714290d66
Arthur <arthur@corenzan.com>
2024-09-16 21:12:30 -0300

    Draft client app

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+	return (
+		<main>
+			<h1>Hello, world</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni itaque
+				quasi, deserunt quidem sint vel deleniti et harum doloribus libero!
+				Deserunt quidem nemo ducimus ullam quod minus obcaecati reprehenderit
+				non.
+			</p>
+		</main>
+	);
+}
